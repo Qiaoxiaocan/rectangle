5427d1c2a044a9da14f285aa8d59fcd57d7b8792
qiaoxiaocan <1293941868@qq.com>
2021-03-04 12:32:09 +0800

    first class of rectangle

---
+++ main.js
@@ -0,0 +1,27 @@
+$(()=> { 
+  //get dom elem
+  let $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calc'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+
+  $btnCal.click(()=> {
+    let w = Number($width.val()),
+        h = Number($height.val());
+
+    let p = (w + h) * 2,
+        a = w * h;
+
+    $perimeter.val(p);
+    $area.val(a);
+  })
+
+  
+  //get value
+  
+  //calc
+  
+  //output
+  
+});
